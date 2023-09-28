ef795b6cf8e3acb73c60d08910de68497c0ab51e
Usman <tusman878@gmail.com>
2023-09-28 11:01:00 +0500

    initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <h1>Usman Web</h1>
+        <p>@all right reserved</p>
+      </div>
+      <div>
+        <h5>Follow Us</h5>
+        <div>
+          <a href="https://www.facebook.com/NmE.UsMaN/" target="blank">
+            Facebook
+          </a>
+          <a href="" target="blank">
+            LinkedIn
+          </a>
+          <a href="https://github.com/Usman-22/Usman-22" target="blank">
+            GitHub
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/Home.jsx
@@ -0,0 +1,89 @@
+import React from "react";
+import vg from "../assets/2.webp";
+import {
+  AiFillGoogleCircle,
+  AiFillAmazonCircle,
+  AiFillYoutube,
+  AiFillInstagram,
+} from "react-icons/ai";
+
+const Home = () => {
+  return (
+    <>
+      <div className="home" id="home">
+        <main>
+          <h1>UsmanWeb</h1>
+          <p>A front end web developer</p>
+        </main>
+      </div>
+      <div className="home2">
+        <img src={vg} alt="Graphics" />
+        <div>
+          <p>
+            We are your one and only solution to the tech problems you face
+            every day. We are leading tech company whose aim is to increase the
+            problem solving ability in children
+          </p>
+        </div>
+      </div>
+      <div className="home3" id="about">
+        <div>
+          <h1>Who we are?</h1>
+          <p>
+            Delivering top-notch web problem-solving services requires a
+            comprehensive approach. It starts with a deep understanding of the
+            issue at hand, gained through active listening and thorough
+            analysis. With technical expertise as your foundation, you can then
+            craft tailored solutions that fit the unique needs of your clients.
+            Clear and transparent communication is essential throughout the
+            process, ensuring your clients comprehend the steps you're taking to
+            resolve their problem. Lastly, prioritize timely responses to
+            maintain client satisfaction, recognizing that swift action is often
+            crucial in the dynamic world of web problem-solving.
+          </p>
+        </div>
+      </div>
+      <div className="home4" id="brands">
+        <div>
+          <h1>Brands</h1>
+          <article>
+            <div
+              style={{
+                animationDelay: "0.3s",
+              }}
+            >
+              <AiFillGoogleCircle />
+              <p>Google</p>
+            </div>
+            <div
+              style={{
+                animationDelay: "0.5s",
+              }}
+            >
+              <AiFillAmazonCircle />
+              <p>Amazon</p>
+            </div>
+            <div
+              style={{
+                animationDelay: "0.7s",
+              }}
+            >
+              <AiFillYoutube />
+              <p>Youtube</p>
+            </div>
+            <div
+              style={{
+                animationDelay: "1s",
+              }}
+            >
+              <AiFillInstagram />
+              <p>Instagram</p>
+            </div>
+          </article>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
